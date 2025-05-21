1fd66eb8ddb4c95886c870019767c7445654a4b4
KrishnaKalra <krishna26kalra@gmail.com>
2025-05-21 14:20:35 +0530

    Added CP_Logger for DOP 2025

---
+++ Cp_Logger/cp-logs.py
@@ -0,0 +1,20 @@
+from platforms.leetcode import get_leetcode_recent_submissions
+from platforms.codeforces import get_codeforces_recent_submissions
+from database_service.mark_data import delete_db
+from database_service.mark_data import mark_db
+import json
+import os
+with open(os.path.join(os.path.dirname(__file__), "questions.json"), "r") as file:
+    questions = json.load(file)
+
+def cpLogs(messageId,studentId,usernameCF,usernameLC,realName,day):
+    leetcodeSubmission=get_leetcode_recent_submissions(usernameLC)
+    codeforcesSubmission=get_codeforces_recent_submissions(usernameCF)
+    questionOfDay=questions[day]
+    print(questionOfDay)
+    mark_db(messageId,studentId,questionOfDay,realName,leetcodeSubmission,codeforcesSubmission,day)
+
+
+
+#cpLogs('B123067','_MShadow_','krishna26kalra','Krishna Kalra','d2')
+delete_db('B123067','d2')
